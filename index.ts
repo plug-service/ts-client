4d5844878f7bf6ce23f057d61954c0b0f463a0a7
zingfeng <thanhdat.finance@gmail.com>
2023-05-12 18:16:27 +0700

    feat: impl logger

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export * from "./src/module/notification";
 export * from "./src/module/base/types/common";
+export * from "./src/module/logger/logger";
---
+++ src/module/logger/logger.ts
@@ -0,0 +1,31 @@
+import { createLogger, format, transports } from "winston";
+
+export enum LOG_LEVEL {
+  ERROR = "error",
+  WARN = "warn",
+  INFO = "info",
+  HTTP = "http",
+  VERBOSE = "verbose",
+  DEBUG = "debug",
+  SILLY = "silly",
+}
+
+/**
+ *
+ * @param logLevel LOG_LEVEL maximum level of messages that should log, defaults to LOG_LEVEL.DEBUG
+ * @returns
+ */
+const logger = (logLevel?: LOG_LEVEL) =>
+  createLogger({
+    level: logLevel || LOG_LEVEL.DEBUG,
+    transports: [new transports.Console()],
+    format: format.combine(
+      format.colorize(),
+      format.timestamp(),
+      format.printf(({ level, message }) => {
+        return `[${level}: ${message}`;
+      })
+    ),
+  });
+
+export default logger;
